0c3c20fc197c00aa317fdedb7c150f278175430d
Nicolas Saraiva <156839933+nicmsaraiva@users.noreply.github.com>
2024-01-17 16:18:46 -0300

    feat: added create method on user controller and updated their service

--- src/main/java/com/nicmsaraiva/demoparkapi/service/UserService.java
+++ src/main/java/com/nicmsaraiva/demoparkapi/service/UserService.java
@@ -1,6 +1,8 @@
 package com.nicmsaraiva.demoparkapi.service;
 
+import com.nicmsaraiva.demoparkapi.entity.User;
 import com.nicmsaraiva.demoparkapi.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +10,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
--- src/main/java/com/nicmsaraiva/demoparkapi/web/controller/UserController.java
+++ src/main/java/com/nicmsaraiva/demoparkapi/web/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.nicmsaraiva.demoparkapi.web.controller;
 
+import com.nicmsaraiva.demoparkapi.entity.User;
 import com.nicmsaraiva.demoparkapi.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody  User user) {
+        User newUser = userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
 }

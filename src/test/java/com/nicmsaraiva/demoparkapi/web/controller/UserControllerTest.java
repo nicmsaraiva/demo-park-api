b95b78d8a2b1a6828e6b822125d87da9e6253810
Nicolas Saraiva <156839933+nicmsaraiva@users.noreply.github.com>
2024-01-22 11:03:34 -0300

    test: added test to user controller get by id

--- src/test/java/com/nicmsaraiva/demoparkapi/web/controller/UserControllerTest.java
+++ src/test/java/com/nicmsaraiva/demoparkapi/web/controller/UserControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
@@ -25,18 +25,32 @@ public class UserControllerTest {
     private MockMvc mockMvc;
 
     ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
     public void UserController_Create_ReturnCreated() throws Exception {
         User user = new User(24L, "nick@test.com", "pwd123", User.Role.ROLE_ADMIN);
         when(userService.save(any())).thenReturn(user);
 
-        String requestBody = objectMapper.writeValueAsString(user);
+        String body = objectMapper.writeValueAsString(user);
 
         mockMvc.perform(post("/api/v1/users")
-                        .content(requestBody)
+                        .content(body)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
-                .andExpect(content().json(requestBody))
+                .andExpect(content().json(body))
                 .andExpect(jsonPath("$.id").value(24L));
+    }
+    @Test
+    public void UserController_GetById_ReturnOk() throws Exception {
+        User user = new User(12L, "joe@test.com", "123pwd", User.Role.ROLE_CLIENT);
+        when(userService.findById(any())).thenReturn(user);
+
+        String responseBody = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(get("/api/v1/users/{id}", user.getId())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseBody))
+                .andExpect(jsonPath("$.id").value(12L));
     }
 }

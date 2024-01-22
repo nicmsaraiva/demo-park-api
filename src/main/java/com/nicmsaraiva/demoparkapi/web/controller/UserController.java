23b8c45bce2eaa0f25f4f0efa2dafb9588f42c1c
Nicolas Saraiva <156839933+nicmsaraiva@users.noreply.github.com>
2024-01-22 11:02:00 -0300

    test: added get users endpoint on user controller

--- src/main/java/com/nicmsaraiva/demoparkapi/web/controller/UserController.java
+++ src/main/java/com/nicmsaraiva/demoparkapi/web/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/users")
@@ -30,5 +32,11 @@ public class UserController {
     public ResponseEntity<User> updatePassword(@PathVariable Long id, @RequestBody User user) {
         User updatedUser = userService.updatePassword(id, user.getPassword());
         return ResponseEntity.ok(updatedUser);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers() {
+        List<User> users = userService.getAll();
+        return ResponseEntity.ok(users);
     }
 }

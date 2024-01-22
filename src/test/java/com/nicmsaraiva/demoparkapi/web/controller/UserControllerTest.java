53ea4aff8c81a140e3229a511a4b08aaa0c72583
Nicolas Saraiva <156839933+nicmsaraiva@users.noreply.github.com>
2024-01-22 11:19:47 -0300

    test: added test to user controller get users

--- src/test/java/com/nicmsaraiva/demoparkapi/web/controller/UserControllerTest.java
+++ src/test/java/com/nicmsaraiva/demoparkapi/web/controller/UserControllerTest.java
@@ -9,7 +9,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -17,6 +20,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(UserController.class)
 
 public class UserControllerTest {
+
+    private final String BASE_URL = "/api/v1/users";
 
     @MockBean
     private UserService userService;
@@ -33,7 +38,7 @@ public class UserControllerTest {
 
         String body = objectMapper.writeValueAsString(user);
 
-        mockMvc.perform(post("/api/v1/users")
+        mockMvc.perform(post(BASE_URL)
                         .content(body)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
@@ -41,16 +46,34 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.id").value(24L));
     }
     @Test
-    public void UserController_GetById_ReturnOk() throws Exception {
+    public void UserController_GetById_ReturnOK() throws Exception {
         User user = new User(12L, "joe@test.com", "123pwd", User.Role.ROLE_CLIENT);
         when(userService.findById(any())).thenReturn(user);
 
         String responseBody = objectMapper.writeValueAsString(user);
 
-        mockMvc.perform(get("/api/v1/users/{id}", user.getId())
+        mockMvc.perform(get(BASE_URL + "/{id}", user.getId())
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().json(responseBody))
                 .andExpect(jsonPath("$.id").value(12L));
+    }
+
+    @Test
+    public void UserController_GetUsers_ReturnOK() throws  Exception {
+        User firstUser = new User(24L, "nick@test.com", "pwd123", User.Role.ROLE_ADMIN);
+        User secondUser = new User(12L, "joe@test.com", "123pwd", User.Role.ROLE_CLIENT);
+        List<User> userList = Arrays.asList(firstUser, secondUser);
+
+        when(userService.getAll()).thenReturn(userList);
+
+        String responseBody = objectMapper.writeValueAsString(userList);
+
+        mockMvc.perform(get(BASE_URL).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseBody))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id").value(24L))
+                .andExpect(jsonPath("$[1].id").value(12L));
     }
 }
